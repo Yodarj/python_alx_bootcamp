5718ce07b7a6f50c29f8fcef1e710944310622d2
Yodarj <jedrzej.antkowiak@gmail.com>
2018-10-06 16:51:20 +0200

    dodatkowe ify

---
+++ zjazd_do_bazy_1/zadanie_12.py
@@ -0,0 +1,13 @@
+i = 0
+
+while i <= 10:
+    print("Ha ha ha")
+    i += 1
+
+while True:
+    instrukcja = input('Wypisać czy zakończyć? [k]')
+    if instrukcja == 'k':
+        break
+    print('haha')
+
+# if 0 == if False == if '' == if lista, gdzie lista = [] == if słownik, gdzie słownik = {}
