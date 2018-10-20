79cd1cfab93f9ad466fbdf0da3217f3ba82d4cd2
Yodarj <jedrzej.antkowiak@gmail.com>
2018-10-20 11:33:22 +0200

    Jedziemy z dychą

--- zjazd_do_bazy_2/kolekcje/zadanie_10.py
+++ zjazd_do_bazy_2/kolekcje/zadanie_10.py
@@ -1,10 +1,13 @@
 napis = input('Podaj nazwę: ')
-
+dictionary = {}
 # napis.count('t')
 #
 # print(napis.count('t'))
 
 
 for i in napis:
-    dictionary = {i:napis.count(i)}
-    print(i, dictionary[i])
+    napis.count(i)
+    dictionary[i] = dictionary.get(i, 0)
+
+print(dictionary)
+#print(i, dictionary[i])
