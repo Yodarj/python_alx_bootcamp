97f7274449a0b510bf200d0a6d7c608ba279f8e9
Yodarj <jedrzej.antkowiak@gmail.com>
2018-10-20 14:08:35 +0200

    Kolejne

---
+++ zjazd_do_bazy_2/operatory zbiorów/zadanie_12_dod.py
@@ -0,0 +1,5 @@
+lista = [1,2,3,1,5,12,2,31,2,51,23,131]
+
+print(min(lista))
+for i in lista:
+    print(min(lista))
---
+++ zjazd_do_bazy_2/operatory zbiorów/zbiory.py
@@ -0,0 +1,16 @@
+zbior = {1, 2, 3, 4}
+
+print(zbior)
+
+zbior.add(1)
+zbior.add('a')
+
+print(zbior)
+
+a = {1, 2, 3}
+b = {3, 4, 5}
+
+print(a | b)
+print(a & b)
+print(a - b)
+print(a ^ b)
