605d2c7230ddb42156f238d7448977588f5162df
Yodarj <jedrzej.antkowiak@gmail.com>
2018-10-20 14:27:10 +0200

    next

--- zjazd_do_bazy_2/operatory zbiorów/zadanie_12_dod.py
+++ zjazd_do_bazy_2/operatory zbiorów/zadanie_12_dod.py
@@ -1,5 +1,19 @@
-lista = [1,2,3,1,5,12,2,31,2,51,23,131]
+lista = [5, 2, 1, 4, 3]
 
-print(min(lista))
-for i in lista:
-    print(min(lista))
+print('Przed: ', lista)
+
+for i in range(len(lista)):
+    index_minimum = i
+    # print(lista[i:])
+    for j in range(i+1, len(lista)):
+        if lista[j] < lista [index_minimum]:
+            index_minimum = j
+    lista[i], lista[index_minimum] = lista[index_minimum], lista[i]
+    print('i: ', i, 'lista: ', lista)
+print('Po: ', lista)
+
+
+
+# print(min(lista))
+# for i in lista:
+#     print(min(lista))
