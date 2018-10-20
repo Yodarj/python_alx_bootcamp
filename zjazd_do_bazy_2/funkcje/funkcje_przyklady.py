7dac28f609c6ea859d15bf48b37d87e9f0cb74f7
Yodarj <jedrzej.antkowiak@gmail.com>
2018-10-20 15:26:45 +0200

    lol

--- zjazd_do_bazy_2/funkcje/funkcje_przyklady.py
+++ zjazd_do_bazy_2/funkcje/funkcje_przyklady.py
@@ -1,14 +1,28 @@
-def przywitaj_sie(x, y, z, c):
-    print('Cześć', x, y, z, c)
+import pytest
 
-lista_osob = [
-    {
-        'x': "Exo",
-        'y': "kuku",
-        'z': "bebe",
-        'c': "asd"
-    }
-]
+# def przywitaj_sie(x, y, z, c):
+#     print('Cześć', x, y, z, c)
+#
+# lista_osob = [
+#     {
+#         'x': "Exo",
+#         'y': "kuku",
+#         'z': "bebe",
+#         'c': "asd"
+#     }
+# ]
+#
+# for osoba in lista_osob:
+#     przywitaj_sie(osoba['x'], osoba['y'], osoba['z'], osoba['c'])
 
-for osoba in lista_osob:
-    przywitaj_sie(osoba['x'], osoba['y'], osoba['z'], osoba['c'])
+
+def czy_wieksza_niz_3(x):
+    if x > 3:
+        return True
+    return False
+
+def test_wieksza_niz_3_dla_wiekszej_niz_3():
+    assert True == czy_wieksza_niz_3(4)
+
+def test_wieksza_niz_3_dla_mniejszej_niz_3():
+    assert False == czy_wieksza_niz_3(2)
--- zjazd_do_bazy_2/funkcje/zad_1.py
+++ zjazd_do_bazy_2/funkcje/zad_1.py
@@ -1,2 +1,11 @@
-asd
-asd
+def czy_jest_pierwsza(x):
+    for i in range(1,x):
+        if x % i == 0 and x == i:
+            print(False)
+            break
+        else:
+            print(True)
+            break
+
+
+czy_jest_pierwsza(10)
