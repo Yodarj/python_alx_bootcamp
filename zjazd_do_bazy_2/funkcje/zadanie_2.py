4df80a061d41a2cca4fd41d33fcfeb02c94f5b68
Yodarj <jedrzej.antkowiak@gmail.com>
2018-10-20 16:23:42 +0200

    dalszejże zadania 2

--- zjazd_do_bazy_2/funkcje/zadanie_2.py
+++ zjazd_do_bazy_2/funkcje/zadanie_2.py
@@ -1,16 +1,39 @@
-def wiecej_niz(napis, x):
-    napis.lower()
+#### moje poniżej poprawne!
+
+# def wiecej_niz(napis, x):
+#     napis.lower()
+#     licznik = {}
+#     zbior = set()
+#     for litera in napis:
+#         licznik[litera] = licznik.get(litera, 0) + 1
+#     for i in licznik:
+#         y = licznik[i]
+#         if y > x:
+#             zbior.add(i)
+#         else:
+#             continue
+#     return zbior
+#
+# def test_wiecej_niz_dla_pustego_napisu():
+#     assert wiecej_niz('',0) == set()
+#
+# def test_wiecej_niz_dla_niepustego_napisu():
+#     assert wiecej_niz('aaaaaaabbccccccd', 2) == {'a', 'c'}
+#
+# print(wiecej_niz('asdawgawda',3))
+
+# ------------------------------------------------------
+
+def wiecej_niz(napis, z):
     licznik = {}
-    zbior = set()
+    wynik = set()
     for litera in napis:
+        litera = litera.lower()
         licznik[litera] = licznik.get(litera, 0) + 1
-    for i in licznik:
-        y = licznik[i]
-        if y > x:
-            zbior.add(i)
-        else:
-            continue
-    return zbior
+    for key, value in licznik.items():
+        if value > z:
+            wynik.add(key)
+    return wynik
 
 def test_wiecej_niz_dla_pustego_napisu():
     assert wiecej_niz('',0) == set()
@@ -18,5 +41,5 @@ def test_wiecej_niz_dla_pustego_napisu():
 def test_wiecej_niz_dla_niepustego_napisu():
     assert wiecej_niz('aaaaaaabbccccccd', 2) == {'a', 'c'}
 
-
-wiecej_niz('aaabbbcccmmmnnnsdoa', 2)
+def test_wiecej_niz_dla_male_duze_litery():
+    assert wiecej_niz('aaaAAAbbccccd', 5) == {'a'}
